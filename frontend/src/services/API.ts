d1707f1d6ba749e3387396a2483efe01d5416e74
Pranjal-mukherjee <pranjal.mukharjee@zemosolabs.com>
2024-03-23 13:26:57 +0530

    backend_deployment

--- frontend/src/services/API.ts
+++ frontend/src/services/API.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_ENDPOINT,
+  baseURL: 'http://localhost:8090',
 });
-console.log(process.env.REACT_APP_API_ENDPOINT);
